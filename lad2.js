771d0cda1d8c8a81c83deb6e3f92a7c7c9b1b56f
timkorn <70910479+timkorn@users.noreply.github.com>
2022-08-17 21:41:04 +0300

    Update lad2.js

--- lad2.js
+++ lad2.js
@@ -1,14 +1,14 @@
 // изменённый js файл
 var formElement = document.forms["formElement"];
 function handleFocus(evt) {
-  var activeElement = document.querySelector(".focused");
+  var activeElement = formElement.querySelector(".focused");
   if (activeElement) {
     activeElement.classList.remove("focused");
   }
   evt.target.classList.add("focused");
 }
 function handleOutFocus(evt) {
-  var activeElement = document.querySelector(".focused");
+  var activeElement = formElement.querySelector(".focused");
   if (activeElement) {
     activeElement.classList.remove("focused");
   }
